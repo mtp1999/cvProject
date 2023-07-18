2d169df15ae46987f51a912a2b779542a6236cf4
mtp1999 <mohamad.tavakolpoorsaleh@gmail.com>
2023-07-18 20:42:31 +0330

    improvements:
    1-add language page
    2-use context in views
    3-use dynamic urls
    4-add media and static settings

--- appCV/urls.py
+++ appCV/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from appCV.views import index
+import appCV.views as views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+app_name = 'appCV'
+
 urlpatterns = [
-    path('', index)
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('skills/', views.skills, name='skills'),
+    path('languages/', views.languages, name='languages'),
+    path('resume/', views.resume, name='resume'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- appCV/views.py
+++ appCV/views.py
@@ -1,5 +1,68 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'appCV/index.html')
+def home(request):
+    context = {
+        'name': 'Mohamad TP',
+    }
+    return render(request, 'appCV/home.html', context)
+
+
+def about(request):
+    context = {
+        'name': 'Mohamad TP',
+        'phone': '+98 911 *** 10 15',
+        'email': 'mohamad.tavakolpoorsaleh@gmail.com',
+        'city': 'Tehran, Iran',
+        'website': 'www.example.com',
+        'age': '24',
+        'degree': 'Bachelor',
+        'freelance': 'available',
+        'birthdate': '21 March 1999',
+
+    }
+    return render(request, 'appCV/about.html', context=context)
+
+
+def languages(request):
+    return render(request, 'appCV/languages.html')
+
+
+def skills(request):
+    context = {
+        'skills': [
+            ['HTML', 100],
+            ['CSS', 90],
+            ['Bootstrap', 90],
+            ['Javascript', 60],
+            ['LPIC1', 80],
+            ['Python', 90],
+            ['PySide6', 80],
+            ['Django', 70],
+            ['Git', 90],
+            ['Docker', 70],
+        ]
+    }
+    return render(request, 'appCV/skills.html', context=context)
+
+
+def resume(request):
+
+    return render(request, 'appCV/resume.html')
+
+
+def contact(request):
+    context = {
+        'location': 'Tehran, Sattari avu, blv Khalil abadi, Mehr-B3',
+        'email': 'mohamad.tavakolpoorsaleh@gmail.com',
+        'phone': '+98 4447 ** *5'
+
+    }
+    return render(request, 'appCV/contact.html', context=context)
+
+
+def services(request):
+    return render(request, 'appCV/services.html')
+
+
+
